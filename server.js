d6cbf14adc4b89f338adcffbfaf68759905f8a0e
naivecoder02 <113258660+naivecoder02@users.noreply.github.com>
2024-04-05 19:14:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,10 +14,13 @@ const port = process.env.PORT || 8001
 mongoose.set('strictQuery', true);
 
 //middlewares
+
 app.use(express.json())
 app.use(cors())
 // console.log(process.env.MONGO_URI);
-
+app.get("/" ,(req,res)=>{
+    res.json("TaskManager");
+})
 //db config
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
